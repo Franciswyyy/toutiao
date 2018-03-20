7aad11b2a5a4a8685ba2a16cb238280c1f6ffb24
王玙 <826531768@qq.com>
2018-03-20 14:55:43 +0800

    service层掉dao

---
+++ src/main/java/com/nowcoder/service/UserService.java
@@ -0,0 +1,17 @@
+package com.nowcoder.service;
+
+import com.nowcoder.dao.UserDao;
+import com.nowcoder.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public User getUser(int id){
+        return userDao.selectById(id);
+    }
+}
