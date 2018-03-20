e45e284e290f6350c54edc3d922033bc0d3df139
王玙 <826531768@qq.com>
2018-03-20 15:09:09 +0800

    xml方式写news表

---
+++ src/main/java/com/nowcoder/dao/NewsDao.java
@@ -0,0 +1,23 @@
+package com.nowcoder.dao;
+
+import com.nowcoder.model.News;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface NewsDao {
+    String TABLE_NAME = "news";
+    String INSERT_FIELDS = " title, link, image, like_count, comment_count, created_date, user_id ";
+    String SELECT_FIELDS = " id, " + INSERT_FIELDS;
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS,
+            ") values (#{title},#{link},#{image},#{likeCount},#{commentCount},#{createdDate},#{userId})"})
+    int addNews(News news);
+
+    //咨询是一批批的，所有可能要用到分页，  通过xml来写sql语句，同样的包名建立xml文件
+    List<News> selectByUserIdAndOffset(@Param("userId") int userId, @Param("offset") int offset,
+                                       @Param("limit") int limit);
+}
