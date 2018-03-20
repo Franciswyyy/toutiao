8767916b032de81a3fba0eec15f63264baea6f70
王玙 <826531768@qq.com>
2018-03-20 14:33:11 +0800

    添加用户

--- src/main/java/com/nowcoder/dao/UserDao.java
+++ src/main/java/com/nowcoder/dao/UserDao.java
@@ -1,5 +1,16 @@
 package com.nowcoder.dao;
 
+import com.nowcoder.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public class UserDao {
+    String TABLE_NAME = "user";
+    String INSET_FIELDS = " name, password, salt, head_url ";
+    String SELECT_FIELDS = " id, name, password, salt, head_url";
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSET_FIELDS,
+            ") values (#{name},#{password},#{salt},#{headUrl})"})
+    int addUser(User user);
 }
