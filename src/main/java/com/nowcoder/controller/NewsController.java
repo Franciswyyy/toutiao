02f3cbb15b6125e258746109a5c331aff83f110f
王玙 <826531768@qq.com>
2018-03-21 13:53:45 +0800

    图片展示到页面 服务器传到客户端

--- src/main/java/com/nowcoder/controller/NewsController.java
+++ src/main/java/com/nowcoder/controller/NewsController.java
@@ -6,11 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 
 @Controller
 public class NewsController {
@@ -19,6 +24,20 @@ public class NewsController {
     @Autowired
     NewsService newsService;
 
+
+    //展示图片， 服务器给客户端 response
+    @RequestMapping(path = {"/image"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public void getImage(@RequestParam("name") String imageName,
+                         HttpServletResponse response) {
+        try {
+            response.setContentType("image/jpeg");
+            StreamUtils.copy(new FileInputStream(new
+                    File(ToutiaoUtil.IMAGE_DIR + imageName)), response.getOutputStream());
+        } catch (Exception e) {
+            logger.error("读取图片错误" + imageName + e.getMessage());
+        }
+    }
 
     @RequestMapping(path = {"/uploadImage/"}, method = {RequestMethod.POST})
     @ResponseBody
